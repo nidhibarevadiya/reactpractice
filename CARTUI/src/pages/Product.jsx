f58dc26ae9bd28e89ce03e0cebe3527131d613b7
Vijay Thalapathy <vijay369@gmail.com>
2025-07-28 14:56:08 +0530

    react

--- CARTUI/src/pages/Product.jsx
+++ CARTUI/src/pages/Product.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// import { addToCart } from "../cart/cartSlice";
+import { selectProducts } from "../cart/cartSelectors";
 
 const Product = () => {
-  return (
-    <div className='head'>Product</div>
-  )
-}
+  const products = useSelector(selectProducts);
+  const dispatch = useDispatch();
 
-export default Product
+  const [productData, setProductData] = useState([]);
+
+  console.log("productData", productData);
+
+  const fetchProduct = async () => {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    setProductData(data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  return (
+    <>
+      <div className="product-container">
+      <div className="product-grid">
+        {productData.map((item,index) => (
+          <div className="product-card" key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <h3>{item.title}</h3>
+            <p className="price">${item.price}</p>
+            <p className="category">{item.category}</p>
+            <p className="rating">Rating: {item.rating?.rate}</p>
+            <p className="description">{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Product;
